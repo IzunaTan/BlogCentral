2cd22e46e2c4d2e248014ae945ad54971b2f2ca8
izunatan <ffxivizunatan@gmail.com>
2019-12-09 17:30:33 +0100

    create sessionlistener class to get the amount of online members, UNTESTED

---
+++ src/main/java/be/intecbrussel/listeners/SessionListener.java
@@ -0,0 +1,25 @@
+package be.intecbrussel.listeners;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SessionListener implements HttpSessionListener {
+    private static List<HttpSession> sessionList = new ArrayList<>();
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        sessionList.add(se.getSession());
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        sessionList.remove(se.getSession());
+    }
+
+    public int amountOfActiveSessions(){
+        return sessionList.size();
+    }
+}
