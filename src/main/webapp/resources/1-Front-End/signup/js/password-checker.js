7185304abcd2fb8d6e4283ad17ba24f390c71d9d
RomuCampu <romucampu@gmail.com>
2019-12-09 15:31:07 +0100

    JS Fiddle

--- src/main/webapp/resources/1-Front-End/signup/js/password-checker.js
+++ src/main/webapp/resources/1-Front-End/signup/js/password-checker.js
@@ -1,14 +1,26 @@
 var password = document.getElementById("password")
-    , confirm_password = document.getElementById("confirm_password");
+  , confirm_password = document.getElementById("confirm_password");
 
-function validatePassword() {
-    if (password.value != confirm_password.value) {
-        confirm_password.setCustomValidity("Passwords Don't Match");
-    } else {
-        confirm_password.setCustomValidity('');
-    }
+
+var isValid = false
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    isValid = false;
+  } else {
+    confirm_password.setCustomValidity('');
+    isValid = true;
+  }
 }
 
-alert('shit doesnt works');
+var removeChars = () => {
+  validatePassword();
+  if (!isValid){
+    confirm_password.value = "";
+    password.value = "";
+  }
+}
+
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
