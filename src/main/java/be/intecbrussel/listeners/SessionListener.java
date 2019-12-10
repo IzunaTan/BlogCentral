22e8bf1efadb9d6c05d968246432631b3a22d18f
izunatan <ffxivizunatan@gmail.com>
2019-12-10 08:39:34 +0100

    WHEN IS HE GONNA STOP COMMENTING !

--- src/main/java/be/intecbrussel/listeners/SessionListener.java
+++ src/main/java/be/intecbrussel/listeners/SessionListener.java
@@ -9,18 +9,22 @@ import java.util.List;
 // TODO testing this thing in one way or another
 // TODO learn about session TTL
 public class SessionListener implements HttpSessionListener {
+    // A list containing all sessions online
     private static List<HttpSession> sessionList = new ArrayList<>();
 
     @Override
+    // When a user is online (session online), add it to the list
     public void sessionCreated(HttpSessionEvent se) {
         sessionList.add(se.getSession());
     }
 
     @Override
+    // When a user is offline (session offline/dead) remove it from the list
     public void sessionDestroyed(HttpSessionEvent se) {
         sessionList.remove(se.getSession());
     }
 
+    // Retrieve the amount of active sessions (people online)
     public static int amountOfActiveSessions(){
         return sessionList.size();
     }
