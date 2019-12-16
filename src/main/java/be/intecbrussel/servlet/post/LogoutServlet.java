1b8294af479a4b64caeb8e87f32c3e448100322c
izunatan <ffxivizunatan@gmail.com>
2019-12-16 07:41:34 +0100

    Set the name of the logout servlet from F*** off to logout

--- src/main/java/be/intecbrussel/servlet/post/LogoutServlet.java
+++ src/main/java/be/intecbrussel/servlet/post/LogoutServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "fuck off", value="/logout")
+@WebServlet(name = "logout", value="/logout")
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
